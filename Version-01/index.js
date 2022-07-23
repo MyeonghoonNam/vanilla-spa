51468e1961e13ed090eab219574cd73cc2e07ec6
MyeongHoonNam <codenmh0822@naver.com>
2022-07-23 17:04:14 +0900

    feat: 가상 DOM 렌더링 구현

--- Version-01/index.js
+++ Version-01/index.js
@@ -1,10 +1,11 @@
 import App from "./src/App.js";
+import applyDiff from "./src/utils/applyDiff.js";
 
 window.setInterval(() => {
   window.requestAnimationFrame(() => {
     const main = document.querySelector(".todoapp");
     const new_main = new App(main).render();
 
-    main.replaceWith(new_main);
+    applyDiff(document.body, main, new_main);
   });
-}, 3000);
+}, 1000);
