c50e72df40661752573cc3667b58d2ce626daf73
MyeongHoonNam <codenmh0822@naver.com>
2022-07-23 16:13:02 +0900

    feat: 동적 데이터 렌더링을 위한 로직

--- Version-01/index.js
+++ Version-01/index.js
@@ -1,8 +1,10 @@
 import App from "./src/App.js";
 
-window.requestAnimationFrame(() => {
-  const main = document.querySelector(".todoapp");
-  const new_main = new App(main).render();
+window.setInterval(() => {
+  window.requestAnimationFrame(() => {
+    const main = document.querySelector(".todoapp");
+    const new_main = new App(main).render();
 
-  main.replaceWith(new_main);
-});
+    main.replaceWith(new_main);
+  });
+}, 3000);
